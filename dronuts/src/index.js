a1146693be999e81f72143a6df73e7e5fbf21cf0
Max Dunaevschi <maxdunaevschi@maxs-air.wifi.local.cmu.edu>
2022-02-09 14:40:21 -0500

    scm: Set up the code to launch frontend when running npm start. Changed framework to use express instead of loopback.

---
+++ dronuts/src/index.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const app = (0, express_1.default)();
+const port = 3001;
+app.use(body_parser_1.default.urlencoded({ extended: false }));
+app.use(body_parser_1.default.json());
+app.get('/', (req, res) => {
+    res.status(200).send('Hello 17-356 People!');
+});
+app.listen(port, () => {
+    console.log(`Todo-App listening on localhost:${port}`);
+});
--- dronuts/src/index.ts
+++ dronuts/src/index.ts
@@ -1,39 +1,15 @@
-import {ApplicationConfig, DronutsApplication} from './application';
+import express from 'express';
+import bodyParser from 'body-parser';
+const app = express();
+const port = 3001;
 
-export * from './application';
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-export async function main(options: ApplicationConfig = {}) {
-  const app = new DronutsApplication(options);
-  await app.boot();
-  await app.start();
+app.get('/', (req, res) => {
+    res.status(200).send('Hello 17-356 People!');
+});
 
-  const url = app.restServer.url;
-  console.log(`Server is running at ${url}`);
-  console.log(`Try ${url}/ping`);
-
-  return app;
-}
-
-if (require.main === module) {
-  // Run the application
-  const config = {
-    rest: {
-      port: +(process.env.PORT ?? 3000),
-      host: process.env.HOST,
-      // The `gracePeriodForClose` provides a graceful close for http/https
-      // servers with keep-alive clients. The default value is `Infinity`
-      // (don't force-close). If you want to immediately destroy all sockets
-      // upon stop, set its value to `0`.
-      // See https://www.npmjs.com/package/stoppable
-      gracePeriodForClose: 5000, // 5 seconds
-      openApiSpec: {
-        // useful when used with OpenAPI-to-GraphQL to locate your application
-        setServersFromRequest: true,
-      },
-    },
-  };
-  main(config).catch(err => {
-    console.error('Cannot start the application.', err);
-    process.exit(1);
-  });
-}
+app.listen(port, () => {
+    console.log(`Todo-App listening on localhost:${port}`);
+});
