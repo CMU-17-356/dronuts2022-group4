bb258164840f9239a1918e3c8393913ffd172556
Max Dunaevschi <maxdunaevschi@Maxs-MacBook-Air.local>
2022-02-09 22:10:18 -0500

    feat/scm: Set up the components folder and created initial components for owner/employee editing inventory page.

--- dronuts/back-end/src/index.js
+++ dronuts/back-end/src/index.js
@@ -7,10 +7,16 @@ const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
 const port = 3001;
+// import fake data
+const fakeDonuts_json_1 = __importDefault(require("../fakeDonuts.json"));
 app.use(body_parser_1.default.urlencoded({ extended: false }));
 app.use(body_parser_1.default.json());
 app.get('/', (req, res) => {
     res.status(200).send('Hello 17-356 People!');
+});
+app.get('/donuts', (req, res) => {
+    // console.log(fakeData);
+    res.status(200).send(fakeDonuts_json_1.default);
 });
 app.listen(port, () => {
     console.log(`Todo-App listening on localhost:${port}`);
--- dronuts/back-end/src/index.ts
+++ dronuts/back-end/src/index.ts
@@ -3,11 +3,19 @@ import bodyParser from 'body-parser';
 const app = express();
 const port = 3001;
 
+// import fake data
+import fakeData from '../fakeDonuts.json';
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.status(200).send('Hello 17-356 People!');
+});
+
+app.get('/donuts', (req, res) => {
+    // console.log(fakeData);
+    res.status(200).send(fakeData);
 });
 
 app.listen(port, () => {
