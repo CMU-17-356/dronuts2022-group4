e8f03459087dfb71470a326a8bab4f8bfc0256d8
Max Dunaevschi <maxdunaevschi@Maxs-MacBook-Air.local>
2022-02-11 11:56:38 -0500

    scm: Added proxy field to package.json in order to make requests to the backend

--- dronuts/front-end/src/components/DonutAvailability.tsx
+++ dronuts/front-end/src/components/DonutAvailability.tsx
@@ -19,64 +19,64 @@ function DonutAvailability() {
       console.log(response);
       setDonutList(response);
     } catch (e) {
-      setDonutList([
-        {
-          "id": 1,
-          "name": "Chocolate Frosting Donut",
-          "img_url": "Img URL here",
-          "available": true
-        },
-        {
-          "id": 2,
-          "name": "Strawberry Frosting Donut",
-          "img_url": "Img URL here",
-          "available": true
-        },
-        {
-          "id": 3,
-          "name": "Test Donut",
-          "img_url": "Img URL here",
-          "available": false
-        },
-        {
-          "id": 4,
-          "name": "Test Donut",
-          "img_url": "Img URL here",
-          "available": true
-        },
-        {
-          "id": 5,
-          "name": "Test Donut",
-          "img_url": "Img URL here",
-          "available": true
-        },
-        {
-          "id": 6,
-          "name": "Test Donut",
-          "img_url": "Img URL here",
-          "available": true
-        },
-        {
-          "id": 7,
-          "name": "Test Donut",
-          "img_url": "Img URL here",
-          "available": false
-        },
-        {
-          "id": 8,
-          "name": "Test Donut",
-          "img_url": "Img URL here",
-          "available": true
-        },
-        {
-          "id": 9,
-          "name": "Test Donut",
-          "img_url": "Img URL here",
-          "available": true
-        }
-      ]
-      )
-      // console.error(e);
+      // setDonutList([
+      //   {
+      //     "id": 1,
+      //     "name": "Chocolate Frosting Donut",
+      //     "img_url": "Img URL here",
+      //     "available": true
+      //   },
+      //   {
+      //     "id": 2,
+      //     "name": "Strawberry Frosting Donut",
+      //     "img_url": "Img URL here",
+      //     "available": true
+      //   },
+      //   {
+      //     "id": 3,
+      //     "name": "Test Donut",
+      //     "img_url": "Img URL here",
+      //     "available": false
+      //   },
+      //   {
+      //     "id": 4,
+      //     "name": "Test Donut",
+      //     "img_url": "Img URL here",
+      //     "available": true
+      //   },
+      //   {
+      //     "id": 5,
+      //     "name": "Test Donut",
+      //     "img_url": "Img URL here",
+      //     "available": true
+      //   },
+      //   {
+      //     "id": 6,
+      //     "name": "Test Donut",
+      //     "img_url": "Img URL here",
+      //     "available": true
+      //   },
+      //   {
+      //     "id": 7,
+      //     "name": "Test Donut",
+      //     "img_url": "Img URL here",
+      //     "available": false
+      //   },
+      //   {
+      //     "id": 8,
+      //     "name": "Test Donut",
+      //     "img_url": "Img URL here",
+      //     "available": true
+      //   },
+      //   {
+      //     "id": 9,
+      //     "name": "Test Donut",
+      //     "img_url": "Img URL here",
+      //     "available": true
+      //   }
+      // ]
+      // )
+      console.error(e);
     }
   }
 
