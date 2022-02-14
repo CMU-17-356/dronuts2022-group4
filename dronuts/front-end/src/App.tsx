c617c3ddf9585c5ce8bf5fddea8c24a6680695bd
AashaiAvadhani1 <aashai123@gmail.com>
2022-02-14 14:53:03 -0500

    changed the app tsx to have the core navigation links in each feature

--- dronuts/front-end/src/App.tsx
+++ dronuts/front-end/src/App.tsx
@@ -3,12 +3,28 @@ import logo from './logo.svg';
 // import './App.css';
 import DonutAvailability from './components/DonutAvailability'
 import OrderStatus from './components/OrderStatus'
+import NavbarScroller from './components/NavbarScroller';
+
+
+//navigation ordering for the navbar
+const navigation = {
+  brand: { name: 'NavScroller', to: '/' },
+  links: [
+    { name: 'Donut Item Availability', to: './components/DonutAvailabilityItem' },
+    { name: 'Donut Availability', to: './components/DonutAvailability' },
+    { name: 'OrderStatus', to: './components/OrderStatus' },
+  ]
+};
 
 function App() {
+
+  //containing the brand and link for each navigation on the navbar
+  const { brand, links } = navigation;
   return (
     <div className="App">
+      <NavbarScroller brand={brand} links={links} />
       <header className="App-header">
-        <OrderStatus />
+        <OrderStatus/>
       </header>
     </div>
   );
