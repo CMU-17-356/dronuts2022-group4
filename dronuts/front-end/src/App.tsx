3f078d21ecc73f72ddb8edba032b895e7c541d6f
AashaiAvadhani1 <aashai123@gmail.com>
2022-02-15 17:41:49 -0500

    merge conflict progress

--- dronuts/front-end/src/App.tsx
+++ dronuts/front-end/src/App.tsx
@@ -3,34 +3,6 @@ import logo from './logo.svg';
 // import './App.css';
 import DonutAvailability from './components/DonutAvailability'
 import OrderStatus from './components/OrderStatus'
-<<<<<<< HEAD
-import NavbarScroller from './components/NavbarScroller';
-import DonutAvailabilityItem from './components/DonutAvailabilityItem';
-import LoadingDroneInstructions from './components/LoadOrders';
-import LoadOrders from './components/LoadOrders';
-
-
-//navigation ordering for the navbar
-const navigation = {
-  brand: { name: 'NavScroller', to: '/' },
-  links: [
-    { name: 'Donut Item Availability', to: './components/DonutAvailabilityItem' },
-    { name: 'Donut Availability', to: './components/DonutAvailability' },
-    { name: 'OrderStatus', to: './components/OrderStatus' },
-    { name: 'Loading Instructions', to: './components/LoadingDroneInstructions' }
-  ]
-};
-
-function App() {
-
-  //containing the brand and link for each navigation on the navbar
-  const { brand, links } = navigation;
-  return (
-    <div className="App">
-      <header className="App-header">
-        <NavbarScroller brand={brand} links={links} />
-        <LoadOrders/>
-=======
 import type { Donut } from './components/DonutStoreItem'
 import DonutStore from './components/DonutStore'
 import DonutStoreCheckout from './components/DonutStoreCheckout'
@@ -44,7 +16,6 @@ function App() {
       <header className="App-header">
         <DonutStore />
         <DonutStoreCheckout donuts={donuts} />
->>>>>>> master
       </header>
     </div>
   );
