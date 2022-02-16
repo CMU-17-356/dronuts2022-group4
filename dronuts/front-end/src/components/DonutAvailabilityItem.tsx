111ba841691ec4dcdbd6df0d676358c972f9775f
Max Dunaevschi <maxdunaevschi@Maxs-MacBook-Air.local>
2022-02-15 19:40:05 -0500

    feat: Added the employee notification system page. Removed dead code from the DonutAvailability and DonutAvailabilityItem page

--- dronuts/front-end/src/components/DonutAvailabilityItem.tsx
+++ dronuts/front-end/src/components/DonutAvailabilityItem.tsx
@@ -1,37 +1,4 @@
-// import React, { useState, useEffect } from 'react';
-// import * as ReactDOM from "react-dom";
-// let Logo ="https://logrocket-assets.io/static/home-hero-c97849b227a3d3015730e3371a76a7f0.svg";
-// function DonutAvailabilityItem() {
-//     const [donut_name, setDonutName] = useState("Chocolate Frosting");
-//     const [isActive, setActive] = useState(true);
-
-//     return (
-//         <div className='App'>
-//           <Text h1>Todo App</Text>
-//           <AddTodoButton />
-//           <DisplayToggle toggleHandler={setShowCompleted} />
-//           <Grid.Container gap={2} justify='center'>
-//             {todoList
-//               ? todoList.map((todo) => {
-//                   if (todo.completed === showCompleted) {
-//                     return (
-//                       <Grid>
-//                         <TodoItem todo={todo} />
-//                       </Grid>
-//                     );
-//                   }
-//                   return null;
-//                 })
-//               : null}
-//           </Grid.Container>
-//         </div>
-//       );
-// }
-//ReactDOM.render(<Hello />, document.getElementById('root'));
-
-
 import React, { useState } from 'react';
-
 // import components
 import { Card, Tag, Text } from '@geist-ui/react';
 import donut_img from '../straw-frosting-donut.png';
@@ -72,7 +39,6 @@ const DonutAvailabilityItem = (props: DonutItemProps) => {
       <Text h4 style={{ textAlign: 'center', paddingRight: '3vw', textTransform: 'uppercase' }}>{donut.name}</Text>
       <img src={donut_img} className="donut-logo" alt="donut" style={{display: 'block', marginLeft: 'auto', marginRight: 'auto', width:'75%', paddingRight: '3vw' }}/>
       <Card.Footer>
-        <Text p>URL: {donut.img_url}</Text>
         <Tag>ID: {donut.id}</Tag>
       </Card.Footer>
     </Card></button>
---
+++ dronuts/front-end/src/components/EmployeeNotificationSystem.tsx
@@ -0,0 +1,132 @@
+import React, { useState, useEffect } from 'react';
+// import './App.css';
+
+// component imports
+import { Page, Text, Card, Divider, Badge, Spacer, Grid, Checkbox, Image } from '@geist-ui/react';
+import drone_img from '../images/drone.png';
+
+function EmployeeNotificationSystem() {
+  const [orderList, setOrderList] = useState<Array<String>>(["o1", "o2", "o3", "o4", "o5"]);
+
+  // TO DO: Set up data retrieval for pages
+  // fetching the async/await way
+//   async function fetchDonuts() {
+//     try {
+//       const response = await fetch('/donuts').then((res) => (res.json()));
+//       console.log(response);
+//       setOrderList(response);
+//     } catch (e) {
+//       console.error(e);
+//     }
+//   }
+
+//   useEffect(() => {
+//     fetchDonuts();
+//   }, []);
+
+  return (
+    <div className='EmpNotSystem'>
+      <Text h2 style={{marginLeft:'1em', marginBottom:'-1em'}}>Pending Orders</Text>
+      <Page>
+        {orderList
+          ? orderList.map((donut) => {
+              return (
+                <div>
+                    <Card width="100%">
+                        <Card.Content>
+                        <Grid.Container gap={2}>
+                        <Grid><Badge type="error">Pending</Badge>  <Spacer h={.5} /></Grid>
+                        <Grid><Text h1 b my={0}>Order #786</Text></Grid>
+                        </Grid.Container>
+                        </Card.Content>
+                        <Divider h="1px" my={0} />
+                        <Grid.Container>
+                            <Grid xs={10}>
+                                <Card.Content>
+                                    <Checkbox checked={false} type="default" scale={2.5}>Chocolate frosting donut x 3</Checkbox>
+                                    <Spacer h={3} />
+                                    <Checkbox checked={false} type="default" scale={2.5}>Strawberry frosting donut x 3</Checkbox>
+                                    <Spacer h={3} />
+                                    <Checkbox checked={true} type="default" scale={2.5}>Vanilla frosting donut with sprinkles x 3</Checkbox>
+                                    <Spacer h={3} />
+                                </Card.Content>
+                            </Grid>
+                            <Grid xs={6} alignContent='center'>
+                                <Card.Content style={{textAlign:'center'}}>
+                                    <Image width="300px" height="160px" draggable={false} src={drone_img} style={{marginBottom:0}}/>
+                                    <Text h3 style={{marginTop:0}}>Drone #3</Text>
+                                </Card.Content>
+                            </Grid>
+                            <Grid xs={8} alignContent='center'>
+                                <Card.Content style={{textAlign:'center'}}>
+                                    <Text h2>Max Duna</Text>
+                                    <Spacer h={0.1} />
+                                    <Text h2>02/15/2022</Text>
+                                    <Spacer h={0.1} />
+                                    <Text h2>ETA ~ 7:43 PM</Text>
+                                </Card.Content>
+                            </Grid>
+                        </Grid.Container>
+                    </Card>
+                    <Spacer h={3} />
+                </div>
+              );
+            return null;
+            })
+          : null}
+      </Page>
+      <Spacer h={3} />
+      <Text h2 style={{marginLeft:'1em', marginBottom:'-1em'}}>Completed Orders</Text>
+      <Page>
+        {orderList
+          ? orderList.map((donut) => {
+              return (
+                <div>
+                    <Card width="100%">
+                        <Card.Content>
+                        <Grid.Container gap={2}>
+                        <Grid><Badge type="success">Completed</Badge>  <Spacer h={.5} /></Grid>
+                        <Grid><Text h1 b my={0}>Order #786</Text></Grid>
+                        </Grid.Container>
+                        </Card.Content>
+                        <Divider h="1px" my={0} />
+                        <Grid.Container>
+                            <Grid xs={10}>
+                                <Card.Content>
+                                    <Checkbox disabled checked={true} type="default" scale={2.5}>Chocolate frosting donut x 3</Checkbox>
+                                    <Spacer h={3} />
+                                    <Checkbox disabled checked={true} type="default" scale={2.5}>Strawberry frosting donut x 3</Checkbox>
+                                    <Spacer h={3} />
+                                    <Checkbox disabled checked={true} type="default" scale={2.5}>Vanilla frosting donut with sprinkles x 3</Checkbox>
+                                    <Spacer h={3} />
+                                </Card.Content>
+                            </Grid>
+                            <Grid xs={6} alignContent='center'>
+                                <Card.Content style={{textAlign:'center'}}>
+                                    <Image width="300px" height="160px" draggable={false} src={drone_img} style={{marginBottom:0}}/>
+                                    <Text h3 style={{marginTop:0}}>Drone #3</Text>
+                                </Card.Content>
+                            </Grid>
+                            <Grid xs={8} alignContent='center'>
+                                <Card.Content style={{textAlign:'center'}}>
+                                    <Text h2>Max Duna</Text>
+                                    <Spacer h={0.1} />
+                                    <Text h2>02/15/2022</Text>
+                                    <Spacer h={0.1} />
+                                    <Text h2>ETA ~ 7:23 PM</Text>
+                                </Card.Content>
+                            </Grid>
+                        </Grid.Container>
+                    </Card>
+                    <Spacer h={3} />
+                </div>
+              );
+            return null;
+            })
+          : null}
+      </Page>
+    </div>
+  );
+}
+
+export default EmployeeNotificationSystem;
