d3198a0865bc5a10126b038889ac397d56c3059d
AashaiAvadhani1 <aashai123@gmail.com>
2022-02-21 13:58:55 -0500

    deleted dead code

--- dronuts/front-end/src/components/NavbarScroller.tsx
+++ dronuts/front-end/src/components/NavbarScroller.tsx
@@ -5,19 +5,6 @@ import { useNavigate } from "react-router-dom";
 
 
 //navigation ordering for the navbar
-const navigation = {
-  brand: { name: 'Home', to: '/' },
-  links: [
-    { name: 'Store', to: '/store' },
-    { name: 'Admin Store', to: '/adminstore' },
-    { name: 'Drones', to: '/drones' },
-    { name: 'Loading Instructions', to: '/loadinstructions' },
-    { name: 'Order Status', to: '/orderstatus' },
-    { name: 'Store Order History', to: '/storeorderhistory' },
-    { name: 'Employee Notification System', to: '/empnotsys' }
-  ]
-};
-
 const NavBarScroller = () => {
   const location = useLocation();
   const navigate = useNavigate();
