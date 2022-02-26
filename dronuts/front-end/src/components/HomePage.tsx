8e999184b06777070b92998eefd84430e2ac4693
Max Dunaevschi <maxdunaevschi@Maxs-MacBook-Air.local>
2022-02-25 19:20:03 -0500

    feat: Added a log in button and made the image an anchor to display all donuts

--- dronuts/front-end/src/components/HomePage.tsx
+++ dronuts/front-end/src/components/HomePage.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 // component imports
 import { Image, Button, Spacer } from '@geist-ui/react';
 import dronutLogoImg from '../images/dronut.png';
+import './HomePageStyle.css'
 
 
 function HomeApp() {
@@ -13,11 +14,16 @@ function HomeApp() {
       navigate('/store');
   }
 
+  function navigateLoginPage() {
+    navigate('/login');
+  }
+
   return ( 
     <div className='HomeApp'>
-        <Image width="100%" src={dronutLogoImg} />
+      <Button auto scale={1.5} type="success" style={{ textTransform: 'uppercase', fontWeight: 'bold', position: 'absolute', top: 10, right: 10 }} onClick={navigateLoginPage}>Log In</Button>
+        <a href='/store' onClick={navigateStore}><Image width="100%" src={dronutLogoImg} /></a>
         <Spacer h={3} />
-        <Button auto scale={1.5} type="success" style={{ textTransform: 'uppercase', fontWeight: 'bold'}} onClick={navigateStore}>View Donuts</Button>
+        {/* <Button auto scale={1.5} type="success" style={{ textTransform: 'uppercase', fontWeight: 'bold'}} onClick={navigateStore}>View Donuts</Button> */}
     </div>
   );
 }
