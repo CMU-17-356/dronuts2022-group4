7c2709df31068fca1e352663fca610aec6335d29
mdunaevs <44279139+mdunaevs@users.noreply.github.com>
2022-02-26 10:19:54 -0500

    fix: Centered and increased the font size of the order type titles. Improved spacing among items on page. (#51)
    Co-authored-by: Max Dunaevschi <maxdunaevschi@Maxs-MacBook-Air.local>

--- dronuts/front-end/src/components/EmployeeNotificationSystem.tsx
+++ dronuts/front-end/src/components/EmployeeNotificationSystem.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-// import './App.css';
 import NavBarScroller from './NavbarScroller';
 import { Page, Text, Spacer } from '@geist-ui/react';
 import EmployeeNotificationSystemOrder from './EmployeeNotificationSystemOrder';
@@ -27,28 +26,26 @@ function EmployeeNotificationSystem() {
   return (
     <div className='EmpNotSystem'>
       <NavBarScroller />
-      <Text h2 style={{marginLeft:'1em', marginBottom:'-1em'}}>Pending Orders</Text>
+      <Text h1 style={{textAlign:'center', marginBottom:'-1em'}}>Pending Orders</Text>
       <Page>
         {orderList
           ? orderList.filter(order => order.status === "pending").map((order) => {
               return (
                 <div>
                     <EmployeeNotificationSystemOrder order={order} />
-                    <Spacer h={3} />
                 </div>
               );
             })
           : null}
       </Page>
       <Spacer h={3} />
-      <Text h2 style={{marginLeft:'1em', marginBottom:'-1em'}}>Completed Orders</Text>
+      <Text h1 style={{textAlign:'center', marginBottom:'-1em'}}>Completed Orders</Text>
       <Page>
         {orderList
           ? orderList.filter(order => order.status === "completed").map((order) => {
               return (
                 <div>
                     <EmployeeNotificationSystemOrder order={order} />
-                    <Spacer h={3} />
                 </div>
               );
             })
