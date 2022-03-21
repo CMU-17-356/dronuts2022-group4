ff2ab0bc1afa1bc9dd419199241845443320acb7
Max Dunaevschi <maxdunaevschi@Maxs-MacBook-Air.local>
2022-03-21 16:32:09 -0400

    feat: Made order status page dynamic with order that was just placed.

--- dronuts/front-end/src/components/DonutStoreCheckout.tsx
+++ dronuts/front-end/src/components/DonutStoreCheckout.tsx
@@ -99,12 +99,13 @@ function DonutStoreCheckout() {
       alert('Please fill out all information.');
       return;
     }
+    let curr_date = new Date()
     let current_order: Order = {
       "id": maxID,
       "customer": currentUser.id,
       "address": address,
       "status": 'Submitted',
-      "purchase_date": new Date(),
+      "purchase_date": curr_date,
       "items": donutCart.map((donut) => [donut.id, donut.quantity]),
     };
     const request = {
@@ -116,7 +117,8 @@ function DonutStoreCheckout() {
       alert('Order submitted!');
       setSubmitted(false);
       setCart(EmptyDonutCart);
-      navigate('/orderstatus');
+      let path = '/orderstatus/' + maxID.toString();
+      navigate(path);
     }, (err) => {
       alert('Unexpected error submitting order.');
     });
