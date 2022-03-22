0c8c82c487092b2208dc4365794803786ccc83b7
Max Dunaevschi <maxdunaevschi@Maxs-MacBook-Air.local>
2022-03-21 21:06:03 -0400

    feat: Added restrictions on what tabs show in navbar depending on user type signed in

--- dronuts/front-end/src/components/NavbarScroller.tsx
+++ dronuts/front-end/src/components/NavbarScroller.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Tabs } from '@geist-ui/react';
 import { useLocation } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
+import User, { EmptyUser } from '../types/User';
+import useLocalStorage from '../util/useLocalStorage';
 
 
 //navigation ordering for the navbar
@@ -9,15 +11,34 @@ const NavBarScroller = () => {
   const location = useLocation();
   const navigate = useNavigate();
 
+  let currentUser: User = useLocalStorage('user', EmptyUser)[0];
+
   return (
     <Tabs value={location.pathname} onChange={(route) => navigate(route, { replace: true })}>
       <Tabs.Item label="Home" value = "/" /> 
       <Tabs.Item label="Store" value = "/store" /> 
-      <Tabs.Item label="Admin Store" value = "/adminstore" />
-      <Tabs.Item label="Order Status" value = "/orderstatus" />
-      <Tabs.Item label="Store Order History" value = "/storeorderhistory" />
-      <Tabs.Item label="Employee Notification System" value = "/empnotsys" />
-      <Tabs.Item label="Drones" value = "/drones" />
+      { currentUser.access_level === 'owner' && 
+        <>
+          <Tabs.Item label="Admin Store" value = "/adminstore" />
+          <Tabs.Item label="Store Order History" value = "/storeorderhistory" />
+        </>
+      }
+      { currentUser.access_level === 'employee' && 
+        <>
+          <Tabs.Item label="Employee Notification System" value = "/empnotsys" />
+          <Tabs.Item label="Drones" value = "/drones" />
+        </>
+      }
+      { currentUser.access_level === 'customer' && 
+        <>
+          <Tabs.Item label="Order Status" value = "/orderstatus" />
+        </>
+      }
+      
+      
+      
+      
+      
     </Tabs>
   );
 };
