2cca2f787cb375da5ca50441991bfaba77d2ae2b
Dennis Lo <dlo2@andrew.cmu.edu>
2022-03-22 11:09:14 -0400

    Add dynamic content to drone status view

--- dronuts/front-end/src/components/DroneStatus.tsx
+++ dronuts/front-end/src/components/DroneStatus.tsx
@@ -1,61 +1,59 @@
+import React from 'react';
 import { Card, Divider, Grid, Image, Progress, Text } from '@geist-ui/react';
 import droneImg from '../images/drone.png';
 import NavBarScroller from './NavbarScroller';
 
 
-function DroneStatus() {
-  const colors = {
+class DroneStatus extends React.Component {
+  colors = {
     33: "red",
     66: "#FFA500",
     100: "green"
-  }
-  
-  return (
-    <div className='HomeApp'>
-    <NavBarScroller />
+  };
 
-    <Grid.Container gap={2}>
-      <Grid xs={24}><Text h2>Drone Status</Text></Grid>
-      <Divider h={5} />
-      <Grid xs={6} justify="center">
-        <Card shadow width="90%">
-          <Image src={droneImg}/>
-          <Text h4>Drone 1</Text>
-          <Text>98% battery</Text>
-          <Progress value={98} colors={colors} width="90%" />
-          <Text type="success">Delivering</Text>
-        </Card>
-      </Grid>
-      <Grid xs={6} justify="center">
-        <Card shadow width="90%">
-          <Image src={droneImg}/>
-          <Text h4>Drone 2</Text>
-          <Text>11% battery</Text>
-          <Progress value={11} colors={colors} width="90%"/>
-          <Text>Returning</Text>
-        </Card>
-      </Grid>
-      <Grid xs={6} justify="center">
-        <Card shadow width="90%">
-          <Image src={droneImg}/>
-          <Text h4>Drone 3</Text>
-          <Text>50% battery</Text>
-          <Progress value={50} colors={colors} width="90%" />
-          <Text>Idle</Text>
-        </Card>
-      </Grid>
-      <Grid xs={6} justify="center">
-        <Card shadow width="90%">
-          <Image src={droneImg}/>
-          <Text h4>Drone 4</Text>
-          <Text>6% battery</Text>
-          <Progress value={6} colors={colors} width="90%" />
-          <Text type="warning">Charging</Text>
-        </Card>
-      </Grid>
-    </Grid.Container>
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      drones: []
+    };
+  }
+
+  async componentDidMount() {
+    const drones_res = await fetch('http://drones.17-356.isri.cmu.edu/airbase/dronuts').then((res) => (res.json()));
+    for (const drone_id of drones_res.body.drones) {
+      // fields: battery capacity, location, and delivery status
+      const drone_res = await fetch(`http://drones.17-356.isri.cmu.edu/drones/${drone_id}`).then((res) => (res.json()));
+      this.setState({
+        drones: this.state.drones.append(drone_res.body)
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className='HomeApp'>
+      <NavBarScroller />
+  
+      <Grid.Container gap={2}>
+        <Grid xs={24}><Text h2>Drone Status</Text></Grid>
+        <Divider h={5} />
+        {this.state.drones.map((drone) => {
+          return (
+            <Grid xs={6} justify="center">
+              <Card shadow width="90%">
+                <Image src={droneImg}/>
+                <Text h4>{drone.id}</Text>
+                <Text>{`${drone.battery}% battery`}</Text>
+                <Progress value={drone.battery} colors={this.colors} width="90%" />
+                <Text type="success">{drone.status}</Text>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid.Container>
+      </div>
+    );
+  }
 }
 
 export default DroneStatus
